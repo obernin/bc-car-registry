674ac28ad13ae484d3fb6881dd0277c9b185f631
Olivier Bernin <OBERNIN@ie.ibm.com>
2016-11-24 18:17:04 +0000

    Added store $ retrieve functionality

--- car_registry.go
+++ car_registry.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
@@ -28,7 +29,16 @@ func (t *CarRegistration) Query(stub shim.ChaincodeStubInterface, function strin
 	
 	fmt.Printf("In CarRegistration.Query: %s, %v", function, args);
 	
-	return nil, nil
+	if len(args) != 1 {
+		return nil, errors.New("{ \"Error\": \"Incorrect number of arguments. Expecting 1: name of the key to read\" }")
+	}
+	
+	valAsbytes, err := stub.GetState(args[0])
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("{ \"Error\": \"Error getting state for key '%s': %s\" }", args[0], err))
+	}
+	
+	return valAsbytes, nil
 }
 
 // Invoke - modify the chaincode data
@@ -36,6 +46,15 @@ func (t *CarRegistration) Query(stub shim.ChaincodeStubInterface, function strin
 func (t *CarRegistration) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	
 	fmt.Printf("In CarRegistration.Invoke: %s, %v", function, args);
+	
+	if len(args) != 2 {
+		return nil, errors.New("{ \"Error\": \"Incorrect number of arguments. Expecting 2: name of the key and value to write\" }")
+	}
+	
+	err := stub.PutState(args[0], []byte(args[1]))
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("{ \"Error\": \"Error setting state for key '%s': %s\" }", args[0], err))
+	}
 	
 	return nil, nil
 }
