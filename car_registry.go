ca41429381d42f37eb73018ce0aaa206460e7737
Olivier Bernin <OBERNIN@ie.ibm.com>
2016-11-30 16:05:42 +0000

    Adding table feature for car registration

--- car_registry.go
+++ car_registry.go
@@ -5,10 +5,18 @@
 package main
 
 import (
+    "bytes" 
 	"errors"
 	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
+
+// A few constants
+
+const TABLE_REGISTRATION 	= "REGISTRATION"
+
+const COLUMN_REGISTRATION	= "REGISTRATION"
+const COLUMN_OWNER			= "OWNER"
 
 
 type CarRegistration struct {
@@ -20,6 +28,26 @@ func (t *CarRegistration) Init(stub shim.ChaincodeStubInterface, function string
 	
 	fmt.Printf("In CarRegistration.Init: %s, %v", function, args);
 	
+	if len(args) != 0 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 0.")
+	}
+	
+	// Creating the table to store the Car Reg and Owner
+	
+	var columnDefsRegTable []*shim.ColumnDefinition
+	columnOneRegTableDef := shim.ColumnDefinition{
+		Name: COLUMN_REGISTRATION, Type: shim.ColumnDefinition_STRING, Key: true}
+	columnTwoRegTableDef := shim.ColumnDefinition{
+		Name: COLUMN_OWNER, Type: shim.ColumnDefinition_STRING, Key: false}
+
+	columnDefsRegTable = append(columnDefsRegTable, &columnOneRegTableDef)
+	columnDefsRegTable = append(columnDefsRegTable, &columnTwoRegTableDef)
+	
+	err := stub.CreateTable(TABLE_REGISTRATION, columnDefsRegTable)
+	if err != nil {
+		return nil, fmt.Errorf("Error creating the registration table: %s", err)
+	}
+	
 	return nil, nil
 }
 
@@ -29,16 +57,13 @@ func (t *CarRegistration) Query(stub shim.ChaincodeStubInterface, function strin
 	
 	fmt.Printf("In CarRegistration.Query: %s, %v", function, args);
 	
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1: name of the key to read")
+	if function == "GetCarOwner" {
+        // update balance
+        return t.GetCarOwner(stub, args)
+    
+	} else {
+		return nil, errors.New("Received unknown function invocation")
 	}
-	
-	valAsbytes, err := stub.GetState(args[0])
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("{ \"Error\": \"Error getting state for key '%s': %s\" }", args[0], err))
-	}
-	
-	return valAsbytes, nil
 }
 
 // Invoke - modify the chaincode data
@@ -47,13 +72,112 @@ func (t *CarRegistration) Invoke(stub shim.ChaincodeStubInterface, function stri
 	
 	fmt.Printf("In CarRegistration.Invoke: %s, %v", function, args);
 	
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2: name of the key and value to write")
+	if function == "RegisterCar" {
+        return t.RegisterCar(stub, args)
+    
+	} else if function == "TransferCar" {
+        return t.TransferCar(stub, args)
+	
+	} else {
+		return nil, errors.New("Received unknown function invocation")
+	}
+}
+
+// Business Logic functions
+
+// Retrieve the owner of a car
+
+func (t *CarRegistration) GetCarOwner(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	
+	fmt.Printf("In CarRegistration.GetCarOwner: %v", args);
+	
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1: car registration")
 	}
 	
-	err := stub.PutState(args[0], []byte(args[1]))
+ 	// Getting the row from the registration
+ 	
+ 	var key []shim.Column
+ 	reg := shim.Column{ Value: &shim.Column_String_{ String_: args[0] } }
+ 	key = append(key, reg)
+ 	row, err := stub.GetRow(TABLE_REGISTRATION, key)
+ 	
+ 	if err != nil {
+	 	return nil, errors.New(fmt.Sprintf("Failed to retrieve record for reg '%s': %s", args[0], err))
+ 	}
+ 	
+ 	// Extracting owner if found and returning
+ 	
+ 	var jsonResp string;
+ 	var buffer bytes.Buffer;
+ 	if (len(row.Columns) != 0) {
+ 		
+ 		owner := row.Columns[1].GetString_()
+ 		
+ 		buffer.WriteString("{ \"registered\": true, \"owner\": " + owner + " }")
+ 		//buffer.WriteString(owner)
+ 		//buffer.WriteString(" }")
+ 		jsonResp = buffer.String()
+
+ 	} else {
+ 		jsonResp = "{ \"registered\": false }"
+ 	}
+ 	
+ 	return []byte(jsonResp), err
+
+}
+
+// Initially registers a car against an owner
+
+func (t *CarRegistration) RegisterCar(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	
+	fmt.Printf("In CarRegistration.RegisterCar: %v", args);
+	
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2: car registration and name of the owner")
+	}
+	
+	// Registers the car - you may want to check first that the car is not already registered
+	
+	_, err := stub.InsertRow(TABLE_REGISTRATION, shim.Row{
+		Columns: []*shim.Column{
+			&shim.Column{Value: &shim.Column_String_{String_: args[0]}},
+			&shim.Column{Value: &shim.Column_String_{String_: args[1]}},
+		},
+	})
+		
 	if err != nil {
-		return nil, errors.New(fmt.Sprintf("{ \"Error\": \"Error setting state for key '%s': %s\" }", args[0], err))
+		return nil, err
+	}
+	
+	return nil, nil
+}
+
+// Transfers a car from an owner to another
+
+func (t *CarRegistration) TransferCar(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	
+	fmt.Printf("In CarRegistration.TransferCar: %v", args);
+	
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2: car registration and name of the new owner")
+	}
+	
+	// Transfers the car ownership
+	
+	updated, err := stub.ReplaceRow(TABLE_REGISTRATION, shim.Row{
+		Columns: []*shim.Column{
+			&shim.Column{Value: &shim.Column_String_{String_: args[0]}},
+			&shim.Column{Value: &shim.Column_String_{String_: args[1]}},
+		},
+	})
+		
+	if err != nil {
+		return nil, err
+	}
+	
+	if updated != true {
+		return nil, errors.New(fmt.Sprintf("No car with registration number '%s'", args[0]))
 	}
 	
 	return nil, nil
